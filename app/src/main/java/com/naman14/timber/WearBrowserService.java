70e102f1fa207027c2386e69fb9f123ed68c342a
Emile Watier <emile.watier@polymtl.ca>
2024-03-07 14:26:09 -0500

    correction Duplicated Code

--- app/src/main/java/com/naman14/timber/WearBrowserService.java
+++ app/src/main/java/com/naman14/timber/WearBrowserService.java
@@ -180,51 +180,25 @@ public class WearBrowserService extends MediaBrowserService {
         }
     }
 
-    private void addMediaRoots(List<MediaBrowser.MediaItem> mMediaRoot) {
+    private void addMediaItem(List<MediaBrowser.MediaItem> mMediaRoot, int type, int title, int subtitle) {
         mMediaRoot.add(new MediaBrowser.MediaItem(
                 new MediaDescription.Builder()
-                        .setMediaId(Integer.toString(TYPE_ARTIST))
-                        .setTitle(getString(R.string.artists))
+                        .setMediaId(Integer.toString(type))
+                        .setTitle(getString(title))
                         .setIconUri(Uri.parse("android.resource://" +
                                 "naman14.timber/drawable/ic_empty_music2"))
-                        .setSubtitle(getString(R.string.artists))
+                        .setSubtitle(getString(subtitle))
                         .build(), MediaBrowser.MediaItem.FLAG_BROWSABLE
         ));
-
-        mMediaRoot.add(new MediaBrowser.MediaItem(
-                new MediaDescription.Builder()
-                        .setMediaId(Integer.toString(TYPE_ALBUM))
-                        .setTitle(getString(R.string.albums))
-                        .setIconUri(Uri.parse("android.resource://" +
-                                "naman14.timber/drawable/ic_empty_music2"))
-                        .setSubtitle(getString(R.string.albums))
-                        .build(), MediaBrowser.MediaItem.FLAG_BROWSABLE
-        ));
-
-        mMediaRoot.add(new MediaBrowser.MediaItem(
-                new MediaDescription.Builder()
-                        .setMediaId(Integer.toString(TYPE_SONG))
-                        .setTitle(getString(R.string.songs))
-                        .setIconUri(Uri.parse("android.resource://" +
-                                "naman14.timber/drawable/ic_empty_music2"))
-                        .setSubtitle(getString(R.string.songs))
-                        .build(), MediaBrowser.MediaItem.FLAG_BROWSABLE
-        ));
-
-
-        mMediaRoot.add(new MediaBrowser.MediaItem(
-                new MediaDescription.Builder()
-                        .setMediaId(Integer.toString(TYPE_PLAYLIST))
-                        .setTitle(getString(R.string.playlists))
-                        .setIconUri(Uri.parse("android.resource://" +
-                                "naman14.timber/drawable/ic_empty_music2"))
-                        .setSubtitle(getString(R.string.playlists))
-                        .build(), MediaBrowser.MediaItem.FLAG_BROWSABLE
-        ));
-
     }
 
-
+    private void addMediaRoots(List<MediaBrowser.MediaItem> mMediaRoot) {
+        addMediaItem(mMediaRoot, TYPE_ARTIST, R.string.artists, R.string.artists);
+        addMediaItem(mMediaRoot, TYPE_ALBUM, R.string.albums, R.string.albums);
+        addMediaItem(mMediaRoot, TYPE_SONG, R.string.songs, R.string.songs);
+        addMediaItem(mMediaRoot, TYPE_PLAYLIST, R.string.playlists, R.string.playlists);
+    }
+    
     private void loadChildren(final String parentId, final Result<List<MediaBrowser.MediaItem>> result) {
 
         final List<MediaBrowser.MediaItem> mediaItems = new ArrayList<>();
